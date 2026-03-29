934be6dc2bfae970dd602b781c504ebb37fcee84
Omkar-Suryawanshi <suryawanshio639@mail.com>
2026-03-30 01:31:35 +0530

    save changes

--- src/main/java/com/project/fitness/security/JwtAuthenticationFilter.java
+++ src/main/java/com/project/fitness/security/JwtAuthenticationFilter.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.io.IOException;
+import java.io.IOException;  // ✅ CORRECT
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
