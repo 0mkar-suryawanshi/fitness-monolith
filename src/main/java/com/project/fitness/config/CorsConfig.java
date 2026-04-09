064eee48932addb57ccd4047f86ab8134bc59ff0
Omkar-Suryawanshi <suryawanshio639@mail.com>
2026-04-10 00:53:03 +0530

    save changes

--- src/main/java/com/project/fitness/config/CorsConfig.java
+++ src/main/java/com/project/fitness/config/CorsConfig.java
@@ -23,7 +23,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(frontendUrl));
+        configuration.setAllowedOrigins(List.of("https://fintness-monolith.netlify.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
